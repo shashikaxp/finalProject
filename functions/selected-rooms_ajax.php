15ec991016c9e170cb684d5e3a0aa84231b3a960
shashika lakmal <shashikaxp@gmail.com>
2016-09-04 19:48:00 +0530

    Edit selected-roms-file

--- functions/selected-rooms_ajax.php
+++ functions/selected-rooms_ajax.php
@@ -4,11 +4,11 @@
 //  $output =  ''   ;
    
 
-	$output =    '<input name="rmid'.$_POST["count"].'" type="text" value="'.$_POST["rmid"].'" >' ;
-	$output .=    '<input name="bed'.$_POST["count"].'" type="text" value="'.$_POST["bed"].'" >' ;
-	$output .=    '<input name="view'.$_POST["count"].'" type="text" value="'.$_POST["view"].'" >' ;
-	$output .=    '<input name="name'.$_POST["count"].'" type="text" value="'.$_POST["name"].'" >' ;
-	$output .=    '<input name="count" type="text" value="'.$_POST["number"].'" >' ;
+	$output =    '<input name="rmid'.$_POST["count"].'" type="hidden" value="'.$_POST["rmid"].'" >' ;
+	$output .=    '<input name="bed'.$_POST["count"].'" type="hidden" value="'.$_POST["bed"].'" >' ;
+	$output .=    '<input name="view'.$_POST["count"].'" type="hidden" value="'.$_POST["view"].'" >' ;
+	$output .=    '<input name="name'.$_POST["count"].'" type="hidden" value="'.$_POST["name"].'" >' ;
+	$output .=    '<input name="count" type="hidden" value="'.$_POST["number"].'" >' ;
 	$output .=    '<div><p> &nbsp;&nbsp;Room '.$_POST["count"].' : '. $_POST["name"] .' - '. $_POST["bed"] .'</p></div>';
   
 
