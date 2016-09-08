8be4149923e29c15fd421ab1ea4cee26b996b387
shashika lakmal <shashikaxp@gmail.com>
2016-09-08 18:05:03 +0530

    edit amenities page

--- Amenities.php
+++ Amenities.php
@@ -12,7 +12,8 @@
 
 
 <div class="col-md-8 col-md-offset-2" style="margin-bottom : 35px;">
-	
+
+<div class="container center">
 	 <?php include "functions/config.php" ?>
 	
 	<?php
@@ -21,7 +22,7 @@
 
 	while($row = mysql_fetch_array($result)) {
 
-	$output	 =	'<div class="amenities col-md-2 center container ">';
+	$output	 =	'<div class="amenities container">';
 	$output	.=	'<h4 >'.$row["name"].'</h4>';
 	$output .=   '<input class="amenities_id" value="'.$row["amenities_id"].'" type="hidden">';
 	$output	.=	'<div calss="container"><img src="images/'.$row["pic"].'" alt="" class="center am-img img img-responsive"></div></div>';
@@ -35,6 +36,7 @@
 
 	?>
 
+	</div>
 </div>
 
 <div id="amanities-modal"></div>
